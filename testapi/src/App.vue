0f58b911ea72b62c2daa142c74b316adb778a0ea
Amirou17 <damirou95@gmail.com>
2021-02-26 12:38:49 +0100

    Changement du nom du fichier

--- testapi/src/App.vue
+++ testapi/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Module1 msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Module1 from './components/Module1.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Module1
   }
 }
 </script>
